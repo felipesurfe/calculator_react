39cd97f8246527dee9a12fa672f5a15d9a678605
felipesurfe <felipe_surfe@hotmail.com>
2022-03-07 14:53:25 -0300

    create css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
 function App() {
+  
+  const createDigits = () => {
+    const digits = [];
+
+    for (let i = 1; i < 10; i++) {
+      digits.push(
+      <button key={i}>{i}</button>
+      )
+    }
+
+    return digits;
+  }
+
   return (
     <div className="App">
       <div class="calculator">
@@ -16,9 +29,12 @@ function App() {
         </div>
 
         <div class="digits">
-
+          { createDigits() }
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>          
         </div>
-        
+
       </div>
     </div>
   );
