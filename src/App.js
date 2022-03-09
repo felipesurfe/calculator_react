ce9dec8c717974d960e4e8a4ca6710698077b1cb
felipesurfe <felipe_surfe@hotmail.com>
2022-03-08 22:12:42 -0300

    add function equal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import { useState } from 'react';
+
 function App() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const ops = ['/', '*', '+', '-', '.'];
+
+  const updateCalc = value => {
+    if (
+      ops.includes(value) && calc === '' ||
+      ops.includes(value) && ops.includes(calc.slice(-1)
+      )
+    ) {
+      return;
+    }
+
+    setCalc(calc + value);
+  }
   
   const createDigits = () => {
     const digits = [];
 
     for (let i = 1; i < 10; i++) {
       digits.push(
-      <button key={i}>{i}</button>
+        <button 
+          onClick={() => updateCalc(i.toString())} 
+          key={i}>
+          {i}
+        </button>
       )
     }
 
@@ -16,22 +38,24 @@ function App() {
     <div className="App">
       <div class="calculator">
         <div class="display">
-          <span>(0)</span> 0
+          {result ? <span>(0)</span> : '' }
+          {calc || "0"}
         </div>
         
         <div class="operators">
-          <button>/</button>
-          <button>*</button>
-          <button>+</button>
-          <button>-</button>
+          <button onClick={() => updateCalc('/')}>/</button>
+          <button onClick={() => updateCalc('*')}>*</button>
+          <button onClick={() => updateCalc('+')}>+</button>
+          <button onClick={() => updateCalc('-')}>-</button>
 
           <button>DEL</button>        
         </div>
 
         <div class="digits">
           { createDigits() }
-          <button>0</button>
-          <button>.</button>
+          <button onClick={() => updateCalc('0')}>0</button>
+          <button onClick={() => updateCalc('.')}>.</button>
+          
           <button>=</button>          
         </div>
 
