aadbd5740aa5d8afa27af2935db6411f8a09064a
felipesurfe <felipe_surfe@hotmail.com>
2022-03-10 22:46:21 -0300

    add favicon

--- src/App.js
+++ src/App.js
@@ -42,6 +42,16 @@ function App() {
     setCalc(eval(calc).toString());
   }
 
+  const deleteLast = () => {
+    if (calc == '') {
+      return;
+    }
+
+    const value = calc.slice(0, -1);
+
+    setCalc(value);
+  }
+
   return (
     <div className="App">
       <div class="calculator">
@@ -56,7 +66,7 @@ function App() {
           <button onClick={() => updateCalc('+')}>+</button>
           <button onClick={() => updateCalc('-')}>-</button>
 
-          <button>DEL</button>        
+          <button onClick={deleteLast}>DEL</button>        
         </div>
 
         <div class="digits">
